a7d239eba361f9f1ee03564cc65325c81ac95bea
Sharat M R <cosmosgenius@gmail.com>
2013-12-25 18:41:41 +0530

    simple code to get list of drivers

--- main.c
+++ main.c
@@ -6,44 +6,21 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-int main(int argc, char* args[])
+int main(int argc, char *argv[])
 {
-    SDL_Window* window = NULL;
-
-    SDL_Surface* screenSurface = NULL;
-
-    if(SDL_Init(SDL_INIT_VIDEO)<0)
+    int i;
+    int num_of_audio_drivers = SDL_GetNumAudioDrivers();
+    printf("Number of audio drivers:- %d", num_of_audio_drivers);
+    for (i = 0; i < num_of_audio_drivers; ++i)
     {
-        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+        const char* driver_name = SDL_GetAudioDriver(i);
+//        if(SDL_AudioInit(driver_name))
+//        {
+//            printf("Audio driver failed to initialize: %s\n", driver_name);
+//            continue;
+//        }
+//        SDL_AudioQuit();
+        printf("\nAudio Driver %d is %s\n", i , driver_name);
     }
-    else
-    {
-        //Create window
-        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-
-        if( window == NULL )
-        {
-            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
-        }
-        else
-        {
-            //Get window surface
-            screenSurface = SDL_GetWindowSurface( window );
-
-            //Fill the surface white
-            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
-
-            //Update the surface
-            SDL_UpdateWindowSurface( window );
-
-            //Wait two seconds
-            SDL_Delay( 2000 );
-        }
-    }
-    //Destroy window
-    SDL_DestroyWindow( window );
-
-    //Quit SDL subsystems
-    SDL_Quit();
     return 0;
 }
