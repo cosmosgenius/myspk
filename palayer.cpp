dfe44ce9b28516b0e85913326245c9ab5e054c59
Sharat M R <cosmosgenius@gmail.com>
2014-05-13 01:55:24 +0530

    added callback function

--- palayer.cpp
+++ palayer.cpp
@@ -15,3 +15,18 @@ bool PaLayer::init()
     }
     return false;
 }
+
+bool PaLayer::start()
+{
+    return true;
+}
+
+bool PaLayer::stop()
+{
+    return true;
+}
+
+int PaLayer::PaCallback(const void *input, void *output, unsigned long frameCount, const PaStreamCallbackTimeInfo *timeInfo, PaStreamCallbackFlags statusFlags, void *userData)
+{
+    return 0;
+}
--- palayer.h
+++ palayer.h
@@ -6,6 +6,14 @@ class PaLayer
 public:
     PaLayer();
     bool init();
+
+private:
+    int PaCallback(
+        const void *input, void *output,
+        unsigned long frameCount,
+        const PaStreamCallbackTimeInfo* timeInfo,
+        PaStreamCallbackFlags statusFlags,
+        void *userData );
 };
 
 #endif // PALAYER_H
